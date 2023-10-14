db607917aa87ed438ae09995fa6d5249a0641d76
Gabriel Sampaio <108162858+GabrielSampaioC@users.noreply.github.com>
2023-10-14 01:24:07 -0300

    Inicio da estilização da lista

--- script.js
+++ script.js
@@ -2,41 +2,47 @@ const botaoAdiciona = document.querySelector("#btn-adiciona-tarefa");
 const input = document.querySelector("#input");
 const corpoTabela = document.querySelector(".tabela-body");
 
-botaoAdiciona.addEventListener("click", () => {
+botaoAdiciona.addEventListener("click", (e) => {
+    e.preventDefault();
     MontaTarefa();
 });
 
 function MontaTarefa() {
+    if(input.value == ""){
+        input.value = "Digite um valor valido";
+    }
+    else{
+        const lista = document.createElement("tr");
+
+        const montaTarefa = document.createElement("td");
+        const dataTarefa = document.createElement("td");
+        const andamentoTarefa = document.createElement("td");
+        const botoes = document.createElement("td");
     
-    const lista = document.createElement("tr");
+        montaTarefa.innerHTML = `<td class="lista-table-resposta">${input.value}</td>`;
+        montaTarefa.classList.add("lista-table-resposta");
+    
+        dataTarefa.innerHTML = `<td class="lista-table-data">hoje</td>`;
+    
+        andamentoTarefa.innerHTML = `<td class="lista-table-opcoes">
+       <select class="lista-table-opcoes">
+           <option>Concluida</option>
+           <option>Em andamento</option>
+           <option>Desisto</option>
+       </select>
+    </td>`;
+    
+        botoes.innerHTML = `<td class="lista-table-botoes">
+        <div>
+            <button class="btn-remove-tarefa"><img src="img/lixeira.svg" class="btn-remove-img" alt="lixeira"></button>
+        </div>
+    </td>`;
+        corpoTabela.appendChild(lista);
+        lista.appendChild(montaTarefa);
+        lista.appendChild(dataTarefa);
+        lista.appendChild(andamentoTarefa);
+        lista.appendChild(botoes);
+    
+    }
+    }
 
-    const MontaTarefa = document.createElement("td");
-    const DataTarefa = document.createElement("td");
-    const andamentoTarefa = document.createElement("td");
-    const botoes = document.createElement("td");
-
-    MontaTarefa.innerHTML = `<td class="lista-table-resposta">${input.value}</td>`;
-
-    DataTarefa.innerHTML = `<td class="lista-table-data">hoje</td>`;
-
-    andamentoTarefa.innerHTML = `<td class="lista-table-opcoes">
-   <select class="lista-table-opcoes">
-       <option>Concluida</option>
-       <option>Em andamento</option>
-       <option>Desisto</option>
-   </select>
-</td>`;
-
-    botoes.innerHTML = `<td class="lista-table-botoes">
-    <div>
-        <button class="btn-editar-tarefa">Edita</button>
-        <button class="btn-remove-tarefa">Remove</button>
-    </div>
-</td>`;
-    corpoTabela.appendChild(lista);
-    lista.appendChild(MontaTarefa);
-    lista.appendChild(DataTarefa);
-    lista.appendChild(andamentoTarefa);
-    lista.appendChild(botoes);
-
-}
