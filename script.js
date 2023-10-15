d7d1b853b2c9ab8f1f969b4e24d3877211c01e0f
Gabriel Sampaio <108162858+GabrielSampaioC@users.noreply.github.com>
2023-10-15 01:55:54 -0300

    Adição do botão para a remoção dos itens.

--- script.js
+++ script.js
@@ -2,47 +2,56 @@ const botaoAdiciona = document.querySelector("#btn-adiciona-tarefa");
 const input = document.querySelector("#input");
 const corpoTabela = document.querySelector(".tabela-body");
 
-botaoAdiciona.addEventListener("click", (e) => {
-    e.preventDefault();
+now = new Date;
+
+botaoAdiciona.addEventListener("click", () => {
     MontaTarefa();
+    input.value = "";
 });
 
 function MontaTarefa() {
-    if(input.value == ""){
-        input.value = "Digite um valor valido";
-    }
-    else{
+    if (input.value == "") {
+        // validaTarefa()
+    } else {
         const lista = document.createElement("tr");
-
         const montaTarefa = document.createElement("td");
         const dataTarefa = document.createElement("td");
         const andamentoTarefa = document.createElement("td");
         const botoes = document.createElement("td");
-    
+
         montaTarefa.innerHTML = `<td class="lista-table-resposta">${input.value}</td>`;
         montaTarefa.classList.add("lista-table-resposta");
-    
-        dataTarefa.innerHTML = `<td class="lista-table-data">hoje</td>`;
-    
+
+        dataTarefa.innerHTML = `<td class="lista-table-data">${now.getDate() + "/" + (now.getMonth() + 1) + "/" + now.getFullYear()}</td>`;
+        
         andamentoTarefa.innerHTML = `<td class="lista-table-opcoes">
-       <select class="lista-table-opcoes">
-           <option>Concluida</option>
-           <option>Em andamento</option>
-           <option>Desisto</option>
-       </select>
-    </td>`;
-    
+            <select class="lista-table-opcoes">
+                <option>Concluida</option>
+                <option>Em andamento</option>
+                <option>Desisto</option>
+            </select>
+        </td>`;
+
         botoes.innerHTML = `<td class="lista-table-botoes">
-        <div>
-            <button class="btn-remove-tarefa"><img src="img/lixeira.svg" class="btn-remove-img" alt="lixeira"></button>
-        </div>
-    </td>`;
+            <div>
+                <button class="btn-remove-tarefa"><img src="img/lixeira.svg" class="btn-remove-img" alt="lixeira"></button>
+            </div>
+        </td>`;
+
         corpoTabela.appendChild(lista);
         lista.appendChild(montaTarefa);
         lista.appendChild(dataTarefa);
         lista.appendChild(andamentoTarefa);
         lista.appendChild(botoes);
-    
-    }
-    }
 
+
+        const botaoRemove = lista.querySelector(".btn-remove-tarefa");
+        botaoRemove.addEventListener("click", () => {
+            RemoveTarefa(lista);
+        });
+    }
+}
+
+function RemoveTarefa(linha) {
+    corpoTabela.removeChild(linha);
+}
