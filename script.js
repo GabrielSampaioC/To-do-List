2421e90f734b8ecbe789b0960d47dc8539bbfee0
Gabriel Sampaio <108162858+GabrielSampaioC@users.noreply.github.com>
2023-10-23 21:18:55 -0300

    Adicionado o LocalStorage ao projeto

--- script.js
+++ script.js
@@ -3,33 +3,83 @@ const input = document.querySelector("#input");
 const corpoTabela = document.querySelector(".tabela-body");
 
 now = new Date;
+let tarefas = [];
+
+if (localStorage.getItem('tarefas')) {
+    tarefas = JSON.parse(localStorage.getItem('tarefas'));
+    renderizaTarefas();
+}
+
+function validaTarefa() {
+    if (input.value.trim().length > 0) {
+        console.log("Tarefa Adicionada");
+        input.classList.remove("error");
+    } else {
+        input.classList.add("error");
+        console.log("Tarefa Removida");
+    }
+}
 
 botaoAdiciona.addEventListener("click", () => {
     MontaTarefa();
+    validaTarefa();
     input.value = "";
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
 });
 
 function MontaTarefa() {
     if (input.value == "") {
-        // validaTarefa()
+
     } else {
+        tarefas.push({
+            nome: input.value,
+            data: now.getDate() + "/" + (now.getMonth() + 1) + "/" + now.getFullYear(),
+            andamento: "Em andamento"
+        });
+
+        localStorage.setItem('tarefas', JSON.stringify(tarefas));
+
+        renderizaTarefas();
+    }
+}
+
+function RemoveTarefa(linha) {
+    const index = Array.from(corpoTabela.children).indexOf(linha);
+    tarefas.splice(index, 1);
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+
+    corpoTabela.removeChild(linha);
+}
+
+function renderizaTarefas() {
+    corpoTabela.innerHTML = "";
+
+    tarefas.forEach((tarefa, index) => {
         const lista = document.createElement("tr");
         const montaTarefa = document.createElement("td");
         const dataTarefa = document.createElement("td");
         const andamentoTarefa = document.createElement("td");
         const botoes = document.createElement("td");
 
-        montaTarefa.innerHTML = `<td class="lista-table-resposta">${input.value}</td>`;
-
-        dataTarefa.innerHTML = `<td class="lista-table-data">${now.getDate() + "/" + (now.getMonth() + 1) + "/" + now.getFullYear()}</td>`;
-        
+        montaTarefa.innerHTML = `<div class="lista-table-resposta">${tarefa.nome}</div>`;
+        dataTarefa.innerHTML = `<td class="lista-table-data">${tarefa.data}</td>`;
         andamentoTarefa.innerHTML = `<td class="lista-table-opcoes">
             <select class="lista-table-opcoes">
-                <option>Concluida</option>
-                <option>Em andamento</option>
-                <option>Desisto</option>
+                <option ${tarefa.andamento === "Concluida" ? "selected" : ""}>Concluida</option>
+                <option ${tarefa.andamento === "Em andamento" ? "selected" : ""}>Em andamento</option>
+                <option ${tarefa.andamento === "Desisto" ? "selected" : ""}>Desisto</option>
             </select>
         </td>`;
+        const select = andamentoTarefa.querySelector("select");
+
+
+        select.addEventListener("change", () => {
+            tarefa.andamento = select.value;
+
+            localStorage.setItem('tarefas', JSON.stringify(tarefas));
+        });
 
         botoes.innerHTML = `<td class="lista-table-botoes">
             <div>
@@ -43,14 +93,9 @@ function MontaTarefa() {
         lista.appendChild(andamentoTarefa);
         lista.appendChild(botoes);
 
-
         const botaoRemove = lista.querySelector(".btn-remove-tarefa");
         botaoRemove.addEventListener("click", () => {
             RemoveTarefa(lista);
         });
-    }
-}
-
-function RemoveTarefa(linha) {
-    corpoTabela.removeChild(linha);
+    });
 }
